e0d676925a91f9445e8f4a49ef416733be0b5710
satyam soni <satyamsoni853@gmail.com>
2025-02-05 19:32:37 +0530

    done20

--- src/ProductDiper/ProductDiper.jsx
+++ src/ProductDiper/ProductDiper.jsx
@@ -4,6 +4,8 @@ import DiperImage1 from '../ProductImage/diper1.jpg'
 import DiperImage2 from '../ProductImage/diper2.jpg'
 import DiperImage3 from '../ProductImage/diper3.jpg'
 import DiperImage4 from '../ProductImage/diper4.jpg'
+import DiperImage6 from '../ProductImage/diper5.jpg'
+import DiperImage8 from '../ProductImage/diper6.jpg'
 import DiperImage5 from '../ProductImage/dipercombo.jpg'
 
 
@@ -17,6 +19,29 @@ function ProductDiper() {
         <div className="card">
           <div className="card-image">
             <img src={DiperImage1} />
+          </div>
+          <div className="card-text">
+            {/* <p className="card-meal-type">Small Diper</p> */}
+            <h2 className="card-title">Big Pack  Diper</h2>
+            <p className="card-body">
+            A Pack Of 40 Diper(Avvailable In Diffrent Size)
+            </p>
+            <button
+              className="Buy-Now-Btn"
+              onClick={() =>
+                (window.location.href =
+                  "https://wa.me/+917007512418?text=" +
+                  encodeURIComponent("Hello"))
+              }
+            >
+              Buy Now
+            </button>
+          </div>
+          <div className="card-price">₹399</div>
+        </div>
+        <div className="card">
+          <div className="card-image">
+            <img src={DiperImage8} />
           </div>
           <div className="card-text">
             {/* <p className="card-meal-type">Small Diper</p> */}
@@ -59,6 +84,29 @@ function ProductDiper() {
             </button>
           </div>
           <div className="card-price">₹99</div>
+        </div>
+        <div className="card">
+          <div className="card-image">
+            <img src={DiperImage6} />
+          </div>
+          <div className="card-text">
+            {/* <p className="card-meal-type">Small Diper</p> */}
+            <h2 className="card-title">Big Pack Diper</h2>
+            <p className="card-body">
+             A Pack Of 40 Diper(Avvailable In Diffrent Size)
+            </p>
+            <button
+              className="Buy-Now-Btn"
+              onClick={() =>
+                (window.location.href =
+                  "https://wa.me/+917007512418?text=" +
+                  encodeURIComponent("Hello"))
+              }
+            >
+              Buy Now
+            </button>
+          </div>
+          <div className="card-price">₹399</div>
         </div>
         <div className="card">
           <div className="card-image">
